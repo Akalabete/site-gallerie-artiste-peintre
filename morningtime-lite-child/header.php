a434b95da79bf0f040bc7d4851c2c44fbf2bf80f
LACOUR Alexandre <lacour_alexandre@proton.me>
2024-04-15 17:22:38 +0200

    finalising rich snippet

--- morningtime-lite-child/header.php
+++ morningtime-lite-child/header.php
@@ -13,12 +13,25 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="URL_DE_VOTRE_SITE">
+	<meta property="og:title" content="TITRE_DE_VOTRE_SITE">
+	<meta property="og:description" content="DESCRIPTION_DE_VOTRE_SITE">
+	<meta property="og:image" content="URL_DE_L_IMAGE">
+	<!-- Instagram -->
+	<meta property="al:ios:url" content="instagram://user?username=VOTRE_NOM_UTILISATEUR">
+	<meta property="al:ios:app_store_id" content="ID_DE_L_APPLICATION">
+	<meta property="al:ios:app_name" content="Instagram">
+	<meta property="al:android:url" content="https://www.instagram.com/_u/VOTRE_NOM_UTILISATEUR">
+	<meta property="al:android:app_name" content="Instagram">
+	<meta property="al:android:package" content="com.instagram.android">
     <script type="application/ld+json">
 {
   "@context": "https://schema.org",
   "@type": "ArtGallery",
   "name": "Hélène LAZOWINSKY - Artiste Peintre Humaniste",
-  "image": "http://peintrehumaniste.local/wp-content/uploads/2024/04/artiste.jpg",
+  "image": "https://peintrehumaniste.fr/wp-content/uploads/2024/04/photocvHL.webp",
   "description": "Galerie Personnelle d'Hélène LAZOWINSKY, Artiste Peintre Humaniste, à Saint Aulaye(24). Découvrez ses oeuvres, ses expositions et ses actualités.",
   "address": {
     "@type": "PostalAddress",
@@ -37,7 +50,7 @@
     {
       "@type": "VisualArtwork",
       "name": "Clownerie II",
-      "image": "http://peintrehumaniste.local/wp-content/uploads/2024/04/Clownerie-II-scaled.jpg",
+      "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/Clownerie-II.webp",
       "description": "portrait d'un homme agé avec un nez de clown rouge",
       "artform": "Peinture",
       "artist": {
@@ -47,16 +60,312 @@
     },
     {
       "@type": "VisualArtwork",
-      "name": "Nom de l'œuvre d'art 2",
-      "image": "URL de l'image de l'œuvre d'art 2",
-      "description": "Description de l'œuvre d'art 2",
+      "name": "Sois-sage-ô-ma-douleur",
+      "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/sois-sage-o-ma-douleur.webp",
+      "description": "portrait 3/4 d'une femme blonde avec une robe rouge",
       "artform": "Peinture",
       "artist": {
         "@type": "Person",
         "name": "Hélène LAZOWINSKY"
       }
-    }
-    // Ajoutez plus d'objets VisualArtwork ici pour chaque œuvre d'art
+    },
+	{
+      "@type": "VisualArtwork",
+      "name": "Mes-nuits-blanches-mes-insomnies",
+      "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/mes-nuits-blanches-mes-insomnies.webp",
+      "description": "portrait d'un homme d'age mur avec sweet a capuche et lunette de soleil",
+      "artform": "Peinture",
+      "artist": {
+        "@type": "Person",
+        "name": "Hélène LAZOWINSKY"
+      }
+    },
+	{
+	  "@type": "VisualArtwork",
+	  "name": "L'homme-qui-murmurait-à-l'oreille-des-corbeaux-corneilles-et-autres-freux-40x40cm",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/L'homme-qui-murmurait-à-l'oreille-des-corbeaux-corneilles-et-autres-freux-40x40cm.webp",
+	  "description": "Portrait d'un himme à barbe avec un corbeau su' l'épaule droite",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Je-crois-pas-que-je-finirais-ma-vie-comme-un-crétin-dans-un-hotel-40x40cm",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/je-crois-pas-que-je-finirais-ma-vie-comme-un-crétin-dans-un-hotel-40x40cm.webp",
+	  "description": "portrait 3/4 d'un homme pensif avec lunette et casquette rouge",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "elucubration_III",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/elucubration_III.webp",
+	  "description": "portrait d'une femme à lunette sombre qui boit un verre de vin",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Elucubration",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/Elucubration.webp",
+	  "description": "portrait d'un homme qui boit un verre de vin",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Elucubration XXL",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/elucubration-XXL.webp",
+	  "description": "portrait d'une personne buvant un verre de vin",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Elucubration VIII",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/Elucubration-VIII.webp",
+	  "description": "portrait d'une femme qui boit un verre",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Elucubration V",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/elucubration-V.webp",
+	  "description": "portrait d'une femme qui boit un verre",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Elucubration I",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/elucubration-I.webp",
+	  "description": "portrait d'une autre femme qui boit un verre",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Clownerie III",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/Clownerie-III.webp",
+	  "description": "portrait d'un homme avec un nez de clown",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Nom de l'œuvre d'art 2",
+	  "image": "URL de l'image de l'œuvre d'art 2",
+	  "description": "Description de l'œuvre d'art 2",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Venise-n'est-pas-en-Italie",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/venise-n'est-pas-en-italie.webp",	
+	  "description": "Jeune femme en train de prendre son café en tenue informelle",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Triste-sire",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/triste-sire.webp",
+	  "description": "homme pensif accoudé à une structure dorée",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "T'es-qu'un-point-sur-le-i-du-chagrin,-de-la-vie",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/tes-qu'un-point-sur-le-i-du-chagrin,-de-la-vie.webp",
+	  "description": "jeune femme en robe noire qui se tient l épaule gauche",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Raison-close-V",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/raison-close-V.webp",
+	  "description": "homme qui se cache la tete sous sa veste",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Olga,-son-dada-c'était-pas-moi-c'était-la-vodka",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/olga,-son-dada-cetait-pas-moi-cetait-la-vodka.webp",
+	  "description": "personne masquée dans un fauteuil en robe de chambre avec un chat noir sur l'accoudoir",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Nous-n'avions-pas-fini-de-nous-parler-d'amour",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/nous-navions-pas-fini-de-nous-parler-damour.webp",
+	  "description": "homme d'age mur barbu assis sur un banc, fond urbain rouge",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Ni-tout-à-fait-la-même-ni-tout-à-fait-une-autre",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/ni-tout-à-fait-la-meme-ni-tout-à-fait-une-autre.webp",
+	  "description": "femme légèrement vétue qui cache l'oeil gauche",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Les-yeux-me-brûlent-d'avoir-pensé-à-pleurer",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/les-yeux-me-brulent-davoir-pensé-à-pleurer.webp",
+	  "description": "jeune femme blonde prostrée sur un dossier sombre",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Le-jour-d'après",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/le-jour-dapres.webp",
+	  "description": "femme habbillée en rouge l'air pensive , assise.",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Là-tout-n'est-qu'ordre-et-beauté-luxe-calme-et-volupté",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/là-tout-nest-quordre-et-beaute-luxe-calme-et-volupte.webp",
+	  "description": "homme chauve avec une boucle d'oreille fumant un cigare à la terrasse d'un café",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Laisse-tes-dents-poser-leur-sourire-de-loup",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/laisse-tes-dents-poser-leur-sourire-de-loup.webp",
+	  "description": "homme nu avec une canne accoudé à quelque chose",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "J'aime-regarder-les-filles",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/jaime-regarder-les-filles.webp",
+	  "description": "deux monsieurs d'âge mur assis sur une banquette d'un café regard perdu dans le décor",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "I-was-a-passenger",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/i-was-a-passenger.webp",
+	  "description": "personne au cheveux longs et clair montrant des doigts avec des bagues dans une posture rock",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Et-ta-soeur",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/et-ta-soeur.webp",
+	  "description": "jeune femme assise sur une chaise rouge",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Elle-dérange-la-nuit-dans-le-bruit-du-silence",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/elle-derange-la-nuit-dans-le-bruit-du-silence.webp",
+	  "description": "dame jouant de la flute habillée de sombre",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	},
+	{
+	  "@type": "VisualArtwork",
+	  "name": "Dessine moi un mouton",
+	  "image": "https://peintrehumaniste.local/wp-content/uploads/2024/04/dessine-moi-un-mouton.webp",
+	  "description": "jeune femme assise près d'une tête de bétail sur une table rouge",
+	  "artform": "Peinture",
+	  "artist": {
+		"@type": "Person",
+		"name": "Hélène LAZOWINSKY"
+	  }
+	}
   ]
 }
 </script>
